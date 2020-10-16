c55b21ef8e9fd675303ab3ff0a615cde30dd4e44
Md. Omar Faruk <iamomar022@gmail.com>
2020-10-16 10:07:08 +0600

    Meals model

---
+++ src/meals/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Meals(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=700)
+    people = models.IntegerField()  # This meal would be enough for how many people
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    preparation_time = models.IntegerField()   # The time this meal to be prepared
+    image = models.ImageField(upload_to='meals/')   # The image for this meal.
