8a58667ea549f19cf87890986bc3a3f3d62fa6d5
Md. Omar Faruk <iamomar022@gmail.com>
2020-10-25 10:01:50 +0600

    Reserve a table

--- src/reservation/views.py
+++ src/reservation/views.py
@@ -5,4 +5,12 @@ from . forms import ReserveTableForm
 # Create your views here.
 
 def reserve_table(request):
-    return render(request, 'reservation/reservation.html')
+    reserve_form = ReserveTableForm()
+
+    if request.method == 'POST':
+        reserve_form = ReserveTableForm(request.POST)
+
+        if reserve_form.is_valid():
+            reserve_form.save()
+
+    return render(request, 'reservation/reservation.html', {'form':reserve_form})
