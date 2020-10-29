f389ac6bdf55886b7576d86a11de40f1d75687fe
Md. Omar Faruk <iamomar022@gmail.com>
2020-10-29 08:46:36 +0600

    routing

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,5 +4,6 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    
+    path('', views.post_list, name='post_list'),
+    path('<int:id>', views.post_detail, name='post_detail'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    pass
+
+
+def post_detail(request, id):
+    pass
