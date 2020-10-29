5dab906c5db2d9d9063b47be10a07d8eb2148df2
Md. Omar Faruk <iamomar022@gmail.com>
2020-10-29 08:56:54 +0600

    Blog Views

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 
 def post_list(request):
-    pass
+    post_list = Post.objects.all()
+
+    return render(request, 'post/post_list.html', {'post_list':post_list})
 
 
 def post_detail(request, id):
-    pass
+    post_detail = Post.objects.get(id=id)
+
+    return render(request, 'post/post_detail.html', {'post_detail':post_detail})
+
