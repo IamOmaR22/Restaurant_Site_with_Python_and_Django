014add19f671f0bd02ab8ef1c2c4260e0d2c8a41
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-07 22:29:04 +0600

    Comment forms

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post, Category, Comment
 from taggit.models import Tag
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -17,11 +18,23 @@ def post_detail(request, id):
 
     comments = Comment.objects.filter(post=post_detail)
 
+    if request.method == 'POST' :
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.post = post_detail
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+
     context = {
         'post_detail' : post_detail,
         'categories' : categories,
         'all_tags' : all_tags,
         'comments' : comments,
+        'comment_form' : comment_form,
     }
 
     return render(request, 'post/post_detail.html', context)
