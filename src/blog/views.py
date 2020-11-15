22c5ad2cae4d87880b69e3aabe0fa7836233881e
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-15 19:27:19 +0600

    Pagination

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from .models import Post, Category, Comment
 from taggit.models import Tag
 from .forms import CommentForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def post_list(request):
     post_list = Post.objects.all()
+
+    paginator = Paginator(post_list, 5) # Show 5 posts per page
+
+    page = request.GET.get('page')
+    post_list = paginator.get_page(page)
 
     return render(request, 'post/post_list.html', {'post_list':post_list})
 
