a381a505791d1cad57d80bd723ddeae885bac94a
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-18 10:46:25 +0600

    routing

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.core.mail import send_mail , BadHeaderError
+from django.http import HttpResponse  , HttpResponseRedirect
+from .forms import ContactForm
 # Create your views here.
+
+
+def send_email(request):
+    pass
+
+
+
+def send_success(request):
+    pass
--- src/restaurant/urls.py
+++ src/restaurant/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('reservation/', include('reservation.urls', namespace='reservation')),
     path('blog/', include('blog.urls', namespace='blog')),
     path('about-us/', include('aboutus.urls', namespace='aboutus')),
+    path('contact/', include('contact.urls', namespace='contact')),
 ] 
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
