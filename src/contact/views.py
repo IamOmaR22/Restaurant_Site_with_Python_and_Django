0140f6da8d180610524cde5d1617029a00c95a3a
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-18 11:01:31 +0600

    forms views

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render , redirect
-from django.core.mail import send_mail , BadHeaderError
-from django.http import HttpResponse  , HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import ContactForm
-# Create your views here.
 
 
 def send_email(request):
-    pass
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass    
+
+    else:
+        form = ContactForm()
+
+    context = {
+        'form' : form
+    }
+
+    return render(request , 'contact/contact.html', context)
 
 
 
 def send_success(request):
-    pass
+    return HttpResponse('thanks you for you email ^_^')
