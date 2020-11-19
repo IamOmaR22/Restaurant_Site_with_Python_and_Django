b355c2a340e5a8b31ef3fcdf5d6f9988a9fd51c4
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-19 12:10:16 +0600

    Home First Part

--- src/home/views.py
+++ src/home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from meals.models import Meals
 
 def home(request):
+
+    meals = Meals.objects.all()
     
     context = {
-        
+        'meals' : meals ,        
     }
 
     return render(request, 'home/index.html', context)
