c57dceac57019a22e2f2c31c600d9991a3fffc5e
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-20 00:20:06 +0600

    Menu and Price

--- src/home/views.py
+++ src/home/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from meals.models import Meals
+from meals.models import Meals, Category
 
 def home(request):
 
     meals = Meals.objects.all()
+    meal_list = Meals.objects.all()
+    categories = Category.objects.all()
     
     context = {
-        'meals' : meals ,        
+        'meals' : meals,      
+        'meal_list' : meal_list,
+        'categories' : categories,  
     }
 
     return render(request, 'home/index.html', context)
