e41007bd576c8801df6eb5bf9c3c1014d89f4761
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-20 00:29:58 +0600

    Blog Post in Home

--- src/home/views.py
+++ src/home/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from meals.models import Meals, Category
+from blog.models import Post
 
 def home(request):
 
     meals = Meals.objects.all()
     meal_list = Meals.objects.all()
     categories = Category.objects.all()
+    posts = Post.objects.all()
     
     context = {
         'meals' : meals,      
         'meal_list' : meal_list,
-        'categories' : categories,  
+        'categories' : categories, 
+        'posts' : posts, 
     }
 
     return render(request, 'home/index.html', context)
