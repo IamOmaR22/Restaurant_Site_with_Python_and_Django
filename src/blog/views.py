39fe3a6dac55321f621b8b81b4fc4f69a676de3f
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-19 10:58:48 +0600

    Search q

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,11 +3,21 @@ from .models import Post, Category, Comment
 from taggit.models import Tag
 from .forms import CommentForm
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 
 def post_list(request):
     post_list = Post.objects.all()
+
+    ## search 
+    search_query = request.GET.get('q')   # q is the name of input field in post_detail.html.
+    if search_query :
+        post_list = post_list.filter(
+            Q(title__icontains = search_query)|
+            Q(content__icontains= search_query) |
+            Q(tags__name__icontains= search_query)
+        ).distinct()
 
     paginator = Paginator(post_list, 5) # Show 5 posts per page
 
