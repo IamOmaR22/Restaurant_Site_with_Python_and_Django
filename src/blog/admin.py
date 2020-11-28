9696c346397982aa278c291638bfeaf8d96da43f
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-29 02:05:26 +0600

    blog post admin panel

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -3,8 +3,12 @@ from .models import Post, Category, Comment
 
 from django_summernote.admin import SummernoteModelAdmin
 
-class BlogPostAdmin(SummernoteModelAdmin):
+class BlogPostAdmin(SummernoteModelAdmin, admin.ModelAdmin):
     summernote_fields = '__all__'
+
+    list_display = ['title', 'author', 'category', 'created']
+    search_fields = ['title', 'content']
+    list_filter = ['category', 'tags']
 
 admin.site.register(Post, BlogPostAdmin)
 admin.site.register(Category)
