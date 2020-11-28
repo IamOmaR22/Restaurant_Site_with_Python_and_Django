182cc7162a73d898411ca9788cbfcc3ed1ad5700
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-29 02:13:43 +0600

    Customizing Project Admin

--- src/meals/admin.py
+++ src/meals/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from . models import Meals, Category
 
-class MealsAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+class MealsAdmin(SummernoteModelAdmin, admin.ModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
+
+    list_display = ['name', 'category', 'people', 'price', 'preparation_time']
+    search_fields = ['name', 'description']
+    list_filter = ['category', 'price']
 
 admin.site.register(Meals, MealsAdmin)
 admin.site.register(Category)
